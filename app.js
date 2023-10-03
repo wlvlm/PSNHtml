a361129c3ffb2e5ec8b97e09e0e7c9f9dd4bd2d8
William Vuillaume <williampgm77@gmail.com>
2023-10-03 12:01:23 +0200

    Ajout tilt

---
+++ app.js
@@ -0,0 +1,26 @@
+let constrain = 80;
+let mouseOverContainer = document.getElementById("container");
+let ex1Layer = document.getElementById("landing");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
+  
+  return "perspective(5000px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer, position);
+  });
+};
